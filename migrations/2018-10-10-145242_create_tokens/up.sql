61c5d806b15a1ed9a216b8b4882bb07d30e24063
Ryan Chipman <ryan@ryanchipman.com>
2018-10-10 10:58:03 -0400

    Add tokens table to schema

---
+++ migrations/2018-10-10-145242_create_tokens/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tokens (
+  token TEXT PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+)
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,20 @@
 table! {
+    tokens (token) {
+        token -> Text,
+        user_id -> Integer,
+    }
+}
+
+table! {
     users (id) {
         id -> Integer,
         email -> Text,
     }
 }
+
+joinable!(tokens -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    tokens,
+    users,
+);
