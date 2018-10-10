2d25e395d59c9dc1bd90a36be33816b786cb5528
Ryan Chipman <ryan@ryanchipman.com>
2018-10-10 19:39:52 -0400

    Add token model

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use diesel::insert_into;
 use diesel::prelude::*;
 use std::error::Error;
-use super::schema::users;
+use super::schema::{tokens,users};
 
 #[derive(Insertable, Serialize, Deserialize, FromForm)]
 #[table_name = "users"]
@@ -32,5 +32,32 @@ impl User {
     pub fn insert(user: &UserInsert, conn: &SqliteConnection) -> Result<usize, impl Error> {
         use super::schema::users::{table as users_table};
         insert_into(users_table).values(user).execute(conn)
+    }
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "tokens"]
+pub struct TokenInsert {
+    pub token: String,
+    pub user_id: i32,
+}
+
+#[derive(Serialize, Queryable, Debug)]
+pub struct TokenQuery {
+    pub token: String,
+    pub user_id: i32,
+}
+
+pub struct Token();
+
+impl Token {
+    pub fn find(token: &String, conn: &SqliteConnection) -> Result<TokenQuery, impl Error> {
+        use super::schema::tokens::dsl::{tokens as all_tokens, token as token_token};
+        all_tokens.filter(token_token.eq(token)).first(conn)
+    }
+
+    pub fn insert(token: &TokenInsert, conn: &SqliteConnection) -> Result<usize, impl Error> {
+        use super::schema::tokens::{table as tokens_table};
+        insert_into(tokens_table).values(token).execute(conn)
     }
 }
