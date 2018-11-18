754008d0cae97674e16de05af585450d4ae9dcab
Ryan Chipman <ryan@ryanchipman.com>
2018-11-17 21:51:27 -0500

    Make most recent migration preserve data

--- migrations/2018-11-16-230311_sensors_add_active/down.sql
+++ migrations/2018-11-16-230311_sensors_add_active/down.sql
@@ -1,7 +1,16 @@
-DROP TABLE sensors;
+ALTER TABLE sensors RENAME TO sensors_tmp;
+
 CREATE TABLE sensors (
   id INTEGER PRIMARY KEY NOT NULL,
   owner_id INTEGER NOT NULL,
   hardware_id INTEGER NOT NULL,
   FOREIGN KEY(owner_id) REFERENCES users(id)
 );
+
+INSERT INTO sensors
+SELECT
+  id, owner_id, hardware_id
+FROM
+  sensors_tmp;
+
+DROP TABLE sensors_tmp;
--- migrations/2018-11-16-230311_sensors_add_active/up.sql
+++ migrations/2018-11-16-230311_sensors_add_active/up.sql
@@ -1,4 +1,5 @@
-DROP TABLE sensors;
+ALTER TABLE sensors RENAME TO sensors_tmp;
+
 CREATE TABLE sensors (
   id INTEGER PRIMARY KEY NOT NULL,
   owner_id INTEGER NOT NULL,
@@ -6,3 +7,11 @@ CREATE TABLE sensors (
   active BOOLEAN NOT NULL DEFAULT 1,
   FOREIGN KEY(owner_id) REFERENCES users(id)
 );
+
+INSERT INTO sensors
+SELECT
+  id, owner_id, hardware_id, 1
+FROM
+  sensors_tmp;
+
+DROP TABLE sensors_tmp;
