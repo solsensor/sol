2e4d71d2f317ad7cff0be3e190d63f84d277a8b9
Ryan Chipman <ryan@ryanchipman.com>
2019-07-08 18:09:22 -0400

    Create a test setup func that resets db

--- src/tests.rs
+++ src/tests.rs
@@ -1,19 +1,23 @@
 use rocket::{http::Status, local::Client};
+use std::fs;
+
+fn setup() -> Client {
+    fs::remove_file("sol.sqlite");
+    super::db::run_migrations();
+    let rocket = super::rocket();
+    Client::new(rocket).expect("created test client")
+}
 
 #[test]
 fn test_simple_ok() {
-    let rocket = super::rocket();
-    let client = Client::new(rocket).expect("valid rocket instance");
+    let client = setup();
     let res = client.get("/").dispatch();
-
     assert_eq!(res.status(), Status::Ok);
 }
 
 #[test]
 fn test_simple_not_ok() {
-    let rocket = super::rocket();
-    let client = Client::new(rocket).expect("valid rocket instance");
+    let client = setup();
     let res = client.get("/invalid_endpoint").dispatch();
-
     assert_eq!(res.status(), Status::NotFound);
 }
