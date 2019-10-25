838ac0cd212ac9ff316d6c0d6a1a5d8423eb2655
Ryan Chipman <ryan@ryanchipman.com>
2019-10-25 17:49:27 -0400

    Use serde json values for tests instead of macros

--- src/tests/api.rs
+++ src/tests/api.rs
@@ -1,98 +1,62 @@
 use crate::{
     api::{AddSensorResponse, GetTokenResponse},
-    tests::util::test_client,
+    json_string,
+    tests::util::{
+        add_sensor, basic_auth_header, get_token, register, response_json_value, test_client,
+        token_auth_header,
+    },
 };
-use rocket::http::{ContentType, Header, Status};
-
-macro_rules! assert_body_json {
-    ($res:ident, $js:tt) => {
-        let contents = $res.body_string().expect("no body");
-        let json = json!($js);
-        assert_eq!(contents, json.to_string());
-    };
-}
-
-macro_rules! assert_res_json {
-    ($res:ident, $status:expr, $js:tt) => {
-        assert_body_json!($res, $js);
-        assert_eq!($res.status(), $status);
-    };
-}
-
-fn basic_auth_header(email: &str, password: &str) -> Header<'static> {
-    let hash = base64::encode(&format!("{}:{}", email, password));
-    Header::new("Authorization", format!("Basic {}", hash))
-}
-
-fn token_auth_header(token: &str) -> Header<'static> {
-    // TODO also test capital bearer
-    Header::new("Authorization", format!("bearer {}", token))
-}
+use rocket::http::{ContentType, Status};
 
 #[test]
 fn get_users_unauthorized() {
     let client = test_client();
     let req = client.get("/api/users/all");
     let mut res = req.dispatch();
-    assert_res_json!(res, Status::BadRequest, {
-        "error": "ApiError(missing token)",
-    });
+    let data = response_json_value(&mut res);
+    let error = data
+        .get("error")
+        .expect("must have 'error' field")
+        .as_str()
+        .expect("value must be string");
+    assert_eq!(error, "ApiError(missing token)");
 }
 
 #[test]
 fn get_token_no_auth_header() {
     let client = test_client();
     let mut res = client.post("/api/token").dispatch();
-    assert_res_json!(res, Status::BadRequest, {
-        "error": "ApiError(missing basic auth header)",
-    });
+    let data = response_json_value(&mut res);
+    let error = data
+        .get("error")
+        .expect("must have 'error' field")
+        .as_str()
+        .expect("value must be string");
+    assert_eq!(error, "ApiError(missing basic auth header)");
 }
 
 #[test]
 fn create_user_get_token() {
     let client = test_client();
-
-    let mut res = client
-        .post("/api/users/new")
-        .header(ContentType::JSON)
-        .body(json!({"email": "newuser@gmail.com", "password": "mypassword"}).to_string())
-        .dispatch();
-    assert_res_json!(res, Status::Ok, {
-        "status": "success",
-        "message": "successfully created user",
-    });
-
-    let mut res = client
-        .post("/api/token")
-        .header(ContentType::JSON)
-        .header(basic_auth_header("newuser@gmail.com", "mypassword"))
-        .body(json!({"email": "newuser@gmail.com", "password": "mypassword"}).to_string())
-        .dispatch();
-    let contents = res.body_string().expect("no body");
-    let data: GetTokenResponse = serde_json::from_str(&contents).expect("failed to deserialize");
-    assert_eq!(&data.token[..5], "user-");
-    assert_eq!(res.status(), Status::Ok);
+    register(&client, "newuser@gmail.com", "mypassword");
+    let tok = get_token(&client, "newuser@gmail.com", "mypassword");
+    assert_eq!(&tok[..5], "user-");
 }
 
 #[test]
-fn add_sensor() {
+fn add_one_sensor() {
     let client = test_client();
+    register(&client, "newuser@gmail.com", "mypassword");
+    let tok = get_token(&client, "newuser@gmail.com", "mypassword");
+    add_sensor(&client, &tok, 12);
+}
 
-    client
-        .post("/api/users/new")
-        .header(ContentType::JSON)
-        .body(json!({"email": "newuser@gmail.com", "password": "mypassword"}).to_string())
-        .dispatch();
-
-    let mut res = client
-        .post("/api/token")
-        .header(ContentType::JSON)
-        .header(basic_auth_header("newuser@gmail.com", "mypassword"))
-        .body(json!({"email": "newuser@gmail.com", "password": "mypassword"}).to_string())
-        .dispatch();
-    let contents = res.body_string().expect("no body");
-    let GetTokenResponse { token: tok } =
-        serde_json::from_str(&contents).expect("failed to deserialize");
+#[test]
+fn add_sensor_duplicate_hardware_id() {
+    let client = test_client();
+    register(&client, "newuser@gmail.com", "mypassword");
+    let tok = get_token(&client, "newuser@gmail.com", "mypassword");
+    add_sensor(&client, &tok, 12);
 
     let mut res = client
         .post("/api/add_sensor")
@@ -100,7 +64,15 @@ fn add_sensor() {
         .header(token_auth_header(&tok))
         .body(json!({"hardware_id": 12}).to_string())
         .dispatch();
-    let contents = res.body_string().expect("no body");
-    let _data: AddSensorResponse = serde_json::from_str(&contents).expect("failed to deserialize");
-    assert_eq!(res.status(), Status::Ok);
+    let data = response_json_value(&mut res);
+    let error = data
+        .get("error")
+        .expect("response should have 'error' field")
+        .as_str()
+        .expect("should be str");
+    assert_eq!(
+        error,
+        "ApiError(active sensor with hardware id 12 already exists)"
+    );
+    assert_eq!(res.status(), Status::BadRequest);
 }
--- src/tests/util.rs
+++ src/tests/util.rs
@@ -1,9 +1,71 @@
 use crate::util::token::rand_str;
-use rocket::local::Client;
+use rocket::{
+    http::{ContentType, Header, Status},
+    local::{Client, LocalResponse},
+};
+
+#[macro_export]
+macro_rules! json_string {
+    ($json:tt) => {
+        serde_json::to_string(&json!($json)).expect("could not stringify json")
+    };
+}
 
 pub fn test_client() -> Client {
     let db_uri = format!("./target/testdbs/{}.db", rand_str());
     crate::db::run_migrations(&db_uri);
     let rocket = crate::rocket(&db_uri, true);
     Client::new(rocket).expect("created test client")
+}
+
+pub fn response_json_value(response: &mut LocalResponse) -> serde_json::Value {
+    let body = response.body().expect("no body");
+    serde_json::from_reader(body.into_inner()).expect("cannot parse response into json")
+}
+
+pub fn basic_auth_header(email: &str, password: &str) -> Header<'static> {
+    let hash = base64::encode(&format!("{}:{}", email, password));
+    Header::new("Authorization", format!("Basic {}", hash))
+}
+
+pub fn token_auth_header(token: &str) -> Header<'static> {
+    // TODO also test capital bearer
+    Header::new("Authorization", format!("bearer {}", token))
+}
+
+pub fn register(client: &Client, email: &str, pass: &str) {
+    let mut res = client
+        .post("/api/users/new")
+        .header(ContentType::JSON)
+        .body(json_string!({"email": email, "password": pass}))
+        .dispatch();
+    let _data = response_json_value(&mut res);
+    assert_eq!(res.status(), Status::Ok);
+}
+
+pub fn get_token(client: &Client, email: &str, pass: &str) -> String {
+    let mut res = client
+        .post("/api/token")
+        .header(ContentType::JSON)
+        .header(basic_auth_header(email, pass))
+        .dispatch();
+    let data = response_json_value(&mut res);
+    let token = data
+        .get("token")
+        .expect("must have a 'token' field")
+        .as_str()
+        .expect("value must be string");
+    assert_eq!(res.status(), Status::Ok);
+    token.to_string()
+}
+
+pub fn add_sensor(client: &Client, token: &str, hw_id: usize) {
+    let mut res = client
+        .post("/api/add_sensor")
+        .header(ContentType::JSON)
+        .header(token_auth_header(token))
+        .body(json!({ "hardware_id": hw_id }).to_string())
+        .dispatch();
+    let _data = response_json_value(&mut res);
+    assert_eq!(res.status(), Status::Ok);
 }
